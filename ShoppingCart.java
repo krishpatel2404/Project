46045dab97114cf5a38aabc0984ea02f9cdabd5f
krishpatel2404 <139245909+krishpatel2404@users.noreply.github.com>
2023-07-15 23:11:20 +0530

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,118 +1,96 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ShoppingCart {
-    private List<Item> cart;
-    private List<Item> favorites;
-    private List<Item> recommendations;
-
-    public ShoppingCart() {
-        cart = new ArrayList<>();
-        favorites = new ArrayList<>();
-        recommendations = new ArrayList<>();
-    }
-
-    public void addToCart(Item item) {
-        cart.add(item);
-        generateRecommendations();
-    }
-
-    public void addToFavorites(Item item) {
-        favorites.add(item);
-    }
-
-    public void viewItemSpecs(Item item) {
-        System.out.println(item.getSpecs());
-    }
-
-    public void generateRecommendations() {
-        // logic to generate recommendations based on items in the cart
-        // for example, if the cart contains a mobile, recommand mobile case
-        // this is just a example, we can implement more
-        recommendations.clear();
-        for(Item item : cart) {
-            if (item.getName().equals("mobile")) {
-                recommendations.add(new Item("Phone Case", "Protective phone case", 40000));
-            } else if(item.getName().equals("TV")) {
-                 recommendations.add(new Item("HDMI cable", "High-quality HDMI cable", 60000));
-            } else if(item.getName().equals("washing machine")) {
-                 recommendations.add(new Item("detergent", "High-efficiency detergent", 200));
-            } else if(item.getName().equals("table cloth")) {
-                 recommendations.add(new Item("placemats", "Matching placemats", 100));
-            } else if(item.getName().equals("bottle")) {
-                 recommendations.add(new Item("bottle", "Cleaning brush for bottles", 1000));
-            } else if(item.getName().equals("mirror")) {
-                 recommendations.add(new Item("cleaning spray", "Streak-free cleaning spray for mirrors", 400));
-            } else if(item.getName().equals("bed sheet")) {
-                 recommendations.add(new Item("pillow cover", "matching pillow cover", 400));
-            } else if(item.getName().equals("bag")) {
-                 recommendations.add(new Item("luggage tag", "personalized luggage tag", 3000));
-            } else if(item.getName().equals("laptop")) {
-                 recommendations.add(new Item("laptop sleeve", "protective laptop sleeve", 70000));
-            } else if(item.getName().equals("earphones")) {
-                 recommendations.add(new Item("earphone case", "protective earphone case", 5000));
-            } else if(item.getName().equals("fan")) {
-                 recommendations.add(new Item("air filter", "cool refreshment air filter", 600));
-            } else if(item.getName().equals("soap")) {
-                 recommendations.add(new Item("soap dish", "plastic soap dish with cap", 30));
-            }
-        }
-    }
-
-    public void viewRecommendations() {
-        for(Item item : recommendations) {
-            System.out.println(item.getName() + ": Rs" + item.getPrice());
-        }
-    }
-
-    public void viewCart() {
-        double total = 0;
-        for(Item item : cart) {
-            System.out.println(item.getName() + ": Rs" + item.getPrice() + "(" + item.getSpecs() + ")");
-            total += item.getPrice();
-        }
-        System.out.println("Total: Rs" + total);
-    }
-    
     public static void main(String[] args) {
-        ShoppingCart cart = new ShoppingCart();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter items to add to cart (type 'done' when finished): ");
+        ArrayList<String> shoppingList = new ArrayList<String>();
+        ArrayList<String> recommendations = new ArrayList<String>();
+        HashMap<String, Double> prices = new HashMap<String, Double>();
+        HashMap<String, String> specs = new HashMap<String, String>();
+        
+        // Pre-defined items and their prices and specs
+        prices.put("mobile", 39999.00);
+        specs.put("mobile", "6.7-inch display, 256GB storage, 108MP+50MP+50MP camera");
+        
+        prices.put("tv", 59999.00);
+        specs.put("tv", "55-inch 4K Ultra HD Smart LED TV");
+        
+        prices.put("washing machine", 15999.00);
+        specs.put("washing machine", "5.2 cu. ft. High-Efficiency Front Load Washer with Steam");
+        
+        prices.put("table cloth", 40.00);
+        specs.put("table cloth", "60 x 84 inches, 100% cotton, Machine washable");
+        
+        prices.put("bottle", 1000.00);
+        specs.put("bottle", "32 oz, Stainless steel, Vacuum insulated");
+        
+        prices.put("mirror", 49.99);
+        specs.put("mirror", "24 x 36 inches, Wall-mounted, Beveled edge");
+        
+        prices.put("bed sheet", 400.00);
+        specs.put("bed sheet", "Queen size, 100% cotton, 400 thread count");
+        
+        prices.put("bag", 3000.00);
+        specs.put("bag", "Leather, Shoulder bag, Zip closure");
+        
+        prices.put("laptop", 99000.00);
+        specs.put("laptop", "15.6-inch display, Intel Core i7, 16GB RAM, 1TB SSD");
+        
+        prices.put("earphones", 5000.00);
+        specs.put("earphones", "In-ear, Noise-cancelling, Wireless");
+        
+        prices.put("fan", 1500.00);
+        specs.put("fan", "16-inch pedestal fan, Adjustable height, Remote control");
+        
+        prices.put("soap", 30.00);
+        specs.put("soap", "4 oz bar, Natural ingredients, Lavender scent");
+        
+        // Pre-defined recommendations for certain items
+        recommendations.add("mobile: Case, Screen protector, Earphones");
+        recommendations.add("tv: Wall mount, HDMI cable, Home theatre");
+        recommendations.add("washing machine: Detergent, Fabric softener");
+        recommendations.add("table cloth: Placemats, Napkins");
+        recommendations.add("bottle: Cleaning brush, Bag, Compass box, Pencil");
+        recommendations.add("mirror: Cleaning spray");
+        recommendations.add("bed sheet: Comforter, Pillow covers");
+        recommendations.add("bag: Wallet, Keychain");
+        recommendations.add("laptop: Mouse, Keyboard, Pendrive, Sleeve");
+        recommendations.add("earphones: Protective case");
+        recommendations.add("fan: Extension cord");
+        recommendations.add("soap: Container");
+        
+        System.out.println("\nEnter item names for shopping (type 'done' when finished): ");
         String input = scanner.nextLine();
-
-        while(!input.equals("buy")) {
-             // add logic to get price and specs of the items from the user or from a database
-            double price = 0;
-            String specs = "";
-            cart.addToCart(new Item(input, specs, price));
+        
+        while (!input.equals("done")) {
+            if (prices.containsKey(input)) {
+                shoppingList.add(input);
+            } else {
+                System.out.println(input + ": Item not available");
+            }
+            
             input = scanner.nextLine();
         }
-        scanner.close();
-        cart.viewCart();
-    }
-}
-
-class Item {
-    private String name;
-    private String specs;
-    private double price;
-
-    public Item(String name, String specs, double price) {
-        this.name = name;
-        this.specs = specs;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSpecs() {
-        return specs;
-    }
-
-    public double getPrice() {
-        return price;
+        
+        System.out.println("\nYour cart: ");
+        
+        for (String item : shoppingList) {
+            System.out.println(item + ": ");
+            System.out.println("\tPrice: Rs." + prices.get(item));
+            System.out.println("\tSpecs: " + specs.get(item));
+            
+            // Print out pre-defined recommendations for the item
+            for (String recommendation : recommendations) {
+                if (recommendation.startsWith(item + ":")) {
+                    System.out.println("\tRecommendations: " + recommendation.substring(recommendation.indexOf(":") + 1).trim());
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Call 9876543210 for customer support.");
+        System.out.println("Thank you!");
     }
 }
